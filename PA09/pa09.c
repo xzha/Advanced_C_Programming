b84033bd2f2fe0a28d6e3761b1381b0cb5eb37fb
xzha <xzha@purdue.edu>
2013-11-07 11:22:12 -0500

    Added in Header

--- PA09/pa09.c
+++ PA09/pa09.c
@@ -1,1 +1,13 @@
 
+int main (int argc, char ** argv)
+{
+  if (argc != 3)
+    {
+      printf("ERROR \n");
+      printf("FORMAT: ./pa09 input out"\n);
+      return EXIT_FAILURE;
+    }
+  
+
+}
+
